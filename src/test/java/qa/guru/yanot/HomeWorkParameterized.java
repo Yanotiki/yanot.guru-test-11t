9ecc8684d6c612f90ad59b03c88c2687e1e57d6c
Yanotiki <yana.fedorenko@soft-machine.ru>
2022-02-03 16:51:19 +0300

    11. Git. Github. Погружаемся. Дмитрий Тучс
    11. Git. Github. Погружаемся. Дмитрий Тучс

--- src/test/java/qa/guru/yanot/HomeWorkParameterized.java
+++ src/test/java/qa/guru/yanot/HomeWorkParameterized.java
@@ -33,9 +33,7 @@ public class HomeWorkParameterized {
     static Stream<Arguments> commonSearchDataProvider() {
         return Stream.of(
                 Arguments.of("Metallica", "Metallica была основана в Лос-Анджелесе 28 октября 1981 года"),
-                Arguments.of("Джеймс Хетфилд", "вокалист и ритм-гитарист метал-группы Metallica"),
-                Arguments.of("Богемская рапсодия", "песня британской рок-группы Queen из альбома A Night at the Opera"),
-                Arguments.of("Фредди Меркьюри", "британский певец парсийского происхождени")
+                Arguments.of("Джеймс Хетфилд", "вокалист и ритм-гитарист метал-группы Metallica")
         );
     }
 
